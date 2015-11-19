e8d5d7bcb9cfd67b2dba7ef092043898016262b1
paren8esis <paren8esis@gmail.com>
2015-11-19 21:21:14 +0200

    Transcribing DNA into RNA.

--- 02_transcribing_DNA_into_RNA.py
+++ 02_transcribing_DNA_into_RNA.py
@@ -1,4 +1,22 @@
+# Copyright 2015 paren8esis
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Open file for reading
+
+#!/usr/bin/env python3
+
 f = open('rosalind_rna.txt', 'r')
 
 # Read all the DNA data
