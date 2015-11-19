aa52b1486df6d5033e5cea71ecac5739e632790c
paren8esis <paren8esis@gmail.com>
2015-11-19 21:27:41 +0200

    Inferring mRNA from protein.

--- 17_inferring_mRNA_from_protein.py
+++ 17_inferring_mRNA_from_protein.py
@@ -1,3 +1,20 @@
+# Copyright 2015 paren8esis
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#!/usr/bin/env python3
+
 codons = {'A': 4, 'C': 2, 'D': 2, 'E': 2, 'F': 2, 'G': 4, 'H': 2, 'I': 3, 'K': 2, 'L': 6, 'M': 1, 'N': 2, 'P': 4, 'Q': 2, 'R': 6, 'S': 6, 'T': 4, 'V': 4, 'W': 1, 'Y': 2}
 
 # Open file for reading
